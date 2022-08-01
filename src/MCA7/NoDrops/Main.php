f5bd2323711d3ab95396219e0811845d14a324a1
MCA7 <abdu77mcpe@gmail.com>
2022-08-02 00:21:36 +0300

    PM4 + PM5 Support

--- src/MCA7/NoDrops/Main.php
+++ src/MCA7/NoDrops/Main.php
@@ -13,13 +13,13 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase implements Listener
 {
 
-	public function onEnable(): void
+	public function onEnable() : void
 	{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 
-	public function onDrop(PlayerDropItemEvent $e): void
+	public function onDrop(PlayerDropItemEvent $e) : void
 	{
 		$config = $this->getConfig();
 		if ($config->get("enabled") === true) {
@@ -28,12 +28,15 @@ class Main extends PluginBase implements Listener
 		}
 	}
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
 	{
-		switch (strtolower($command->getName())) {
+		switch(strtolower($command->getName())) {
+
 			case 'toggledrops':
-				if (!get_class($sender) == "pocketmine\player\Player" or $sender->hasPermission("nodrop.command")) {
-					if ($this->getConfig()->get("enabled") === true) {
+
+				if(!get_class($sender) == "pocketmine\player\Player" or $sender->hasPermission("nodrop.command")) {
+					if($this->getConfig()->get("enabled") === true) {
 						$this->getConfig()->set("enabled", false);
 						$this->getConfig()->save();
 						$sender->sendMessage("§aToggled drops (disabled)");
@@ -46,35 +49,45 @@ class Main extends PluginBase implements Listener
 					$sender->sendMessage("§cYou do not have the permission!");
 				}
 				return true;
+
 			case 'trash':
 			case 'trashbin':
-				if (get_class($sender) == "pocketmine\player\Player") {
+
+				if(get_class($sender) == "pocketmine\player\Player") {
 					$trash = $sender->getInventory()->getItemInHand();
+					if($trash->isNull()) {
+						$sender->sendMessage('§cHold the trash!');
+						return true;
+					}
 					$sender->getInventory()->removeItem($trash);
 					$sender->sendMessage("§aRemoved item in hand to trashbin!");
 				} else {
 					$sender->sendMessage("Execute in-game!");
 				}
 				return true;
+
 			case 'dropitem':
 			case 'drop':
-				if (get_class($sender) == "pocketmine\player\Player") {
-					if ($this->getConfig()->get("drop-command") === true) {
-						$item = $sender->getInventory()->getItemInHand();
-						if (!$item->getId() == 0) {
-							$sender->getInventory()->removeItem($item);
-							$sender->dropItem($item);
-							$sender->sendMessage("§aItem dropped!");
-						} else {
-							$sender->sendMessage("§cHold the item that you want to drop!");
-						}
-					} else {
-						$sender->sendMessage("§cThis command has been disabled by the server admin§e!");
-					}
-				} else {
+
+				if(get_class($sender) == "pocketmine\player\Player") { 
 					$sender->sendMessage("Command is only executable in-game!");
+					return true;
 				}
+				if($this->getConfig()->get("drop-command") === false) { 
+					$sender->sendMessage("§cThis command has been disabled by the server admin§e!");
+					return true;
+				}
+				$item = $sender->getInventory()->getItemInHand();
+				if($item->isNull()) { 
+					$sender->sendMessage("§cHold the item that you want to drop!");
+					return true;
+				}
+				$sender->getInventory()->removeItem($item);
+				$sender->dropItem($item);
+				$sender->sendMessage("§aItem dropped!");
+									
 		}
 		return true;
 	}
+
 }
